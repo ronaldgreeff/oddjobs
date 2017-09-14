8ddb99396e6c96ba68afd604c85dfa23e576be0b
ronaldgreeff <ronald.greeff@gmail.com>
2017-09-14 09:12:43 +0100

    Users can now add jobs, generating listings

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Listing
 from .forms import ListingForm
@@ -14,5 +14,14 @@ def listing_detail(request, pk):
 	return render(request, 'listings/listing_detail.html', {'listing': listing})
 
 def listing_new(request):
-	form = ListingForm()
-	return render(request, 'listings/listing_edit.html', {'form': form})
+	if request.method == "POST":
+		form = ListingForm(request.POST)
+		if form.is_valid():
+			listing = form.save(commit=False)
+			listing.listee = request.user
+			listing.created_date = timezone.now()
+			listing.save()
+			return redirect('listing_detail', pk=listing.pk)
+	else:
+		form = ListingForm()
+		return render(request, 'listings/listing_edit.html', {'form': form})
